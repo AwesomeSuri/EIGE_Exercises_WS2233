494923b926e7634eafcd212f01f70f1ade95ec1c
Akbar Suriaganda <akbar.suriaganda@gmail.com>
2022-11-15 00:00:42 +0100

    E2: Hit and debug

--- SpaceShooter/Assets/Scripts/Projectile.cs
+++ SpaceShooter/Assets/Scripts/Projectile.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 
 public class Projectile : MonoBehaviour
@@ -7,5 +8,17 @@ public class Projectile : MonoBehaviour
     private void Update()
     {
         transform.Translate(Vector3.up * (Time.deltaTime * speed));
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Enemy"))
+        {
+            var enemy = other.GetComponent<Enemy>();
+            if (enemy != null)
+            {
+                Debug.Log("We hit: " + other.name);
+            }
+        }
     }
 }
